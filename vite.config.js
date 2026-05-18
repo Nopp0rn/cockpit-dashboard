9c3b67dc68ca8058513ed05e89eebe27acc12090
Noppone Sripongpran <169129646+Nopp0rn@users.noreply.github.com>
2026-05-18 13:28:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+
+  build: {
+    target: 'esnext',
+
+    chunkSizeWarningLimit: 2000,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+
+          // React
+          if (
+            id.includes('react') ||
+            id.includes('react-dom')
+          ) {
+            return 'react-vendor'
+          }
+
+          // Charts
+          if (id.includes('recharts')) {
+            return 'charts'
+          }
+
+          // Excel
+          if (id.includes('xlsx')) {
+            return 'excel'
+          }
+
+          // Supabase
+          if (id.includes('@supabase')) {
+            return 'supabase'
+          }
+
+          // node_modules อื่น
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  },
+
+  server: {
+    host: true,
+    port: 5173
+  }
+})

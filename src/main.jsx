e0a3ef09653955a20388e478342d251f0bd9263c
Noppone Sripongpran <169129646+Nopp0rn@users.noreply.github.com>
2026-05-18 13:35:46 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode><App /></StrictMode>
+  <StrictMode>
+    <Suspense fallback={
+      <div style={{
+        color:'#fff',
+        padding:'40px',
+        fontFamily:'sans-serif'
+      }}>
+        Loading Cockpit Dashboard...
+      </div>
+    }>
+      <App />
+    </Suspense>
+  </StrictMode>
 )
